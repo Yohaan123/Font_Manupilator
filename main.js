5ca3696bfab6acde6338e08bffa5c45793ca6b7d
Yohaan <80580077+Yohaan123@users.noreply.github.com>
2021-08-27 09:57:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-function setup(){
-    canvas = createCanvas(500, 500);
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
     canvas.position(560, 115);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log(" PoseNet is initialized ");
+}
+
+function draw() {
+    background('#f08800');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log("results = " + results);
+    }
 }
