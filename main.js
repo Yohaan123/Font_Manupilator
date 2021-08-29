9b28177ae674298164a0c1f0a7faefcfb37d1d58
Yohaan <80580077+Yohaan123@users.noreply.github.com>
2021-08-29 15:53:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -13,12 +17,21 @@ function modelLoaded() {
     console.log(" PoseNet is initialized ");
 }
 
-function draw() {
-    background('#f08800');
-}
 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log("results = " + results);
+
+        leftWristX = results[0].pose.leftWrist.x
+        rightWristX = results[0].pose.rightWrist.x
+        difference = floor(leftWristX - rightWristX);
     }
 }
+
+function draw() {
+    background('#f08800');
+    fill();
+    textSize(difference);
+    text('Yohaan',50,400);
+}
+
